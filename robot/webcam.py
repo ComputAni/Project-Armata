6eb5b07a4c8e615ba34d04b39180ee17a8b77445
Yong Zhao <yongyizhao@gmail.com>
2017-10-29 21:00:52 +0000

    Modified webcam code to take discrete images

--- robot/webcam.py
+++ robot/webcam.py
@@ -10,14 +10,11 @@ cap = cv2.VideoCapture(cam_index) # Video capture object
 
 
 cap.open(cam_index) # Enable the camera
-while True:
+for i in range(10):
     ret, frame = cap.read() # gets one frame from the webcam
     if frame is not None:
         # Resizing the frame! Ignore dsize. fx scales the width, fy scales the height
         frame = cv2.resize(frame, dsize=(0,0), fx=1.5,fy=1.5)
         cv2.imshow(window_name, frame)
-    k = cv2.waitKey(10) & 0xFF
-    if k == 27: # Escape key
-        cv2.destroyAllWindows()
-        cap.release()
-        break
+    if i == 9:
+        cv2.imwrite('captured.jpg', frame)
