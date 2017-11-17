e5ca0685c96a0f5320990687dc71d09cfce6f3e5
Yong Zhao <yongyizhao@gmail.com>
2017-11-17 05:26:48 +0000

    Playing around with linear movement

--- integrated/testMove.py
+++ integrated/testMove.py
@@ -9,15 +9,15 @@ c = threadDrive.motor(31, 33, 35, 37)
 d = threadDrive.motor(3, 5, 7, 11)
 motorL = [a, b, c, d]
 
-# # Test 1: Move forward 10 steps
-# for i in range(10):
-#     threadDrive.forward(motorL, 5600)
+# Test 1: Move forward 10 steps
+for i in range(10):
+    print("Step number: " + str(i + 1))
+    threadDrive.forward(motorL, 5600)
 
-
-# Test 2: Rotations
-for i in range(5):
-  print("Starting rotation: " + str(i+1))
-  threadDrive.cw(motorL, 2400)
-  threadDrive.cw(motorL, -2400)
+# # Test 2: Rotations
+# for i in range(5):
+#   print("Starting rotation: " + str(i+1))
+#   threadDrive.cw(motorL, 2400)
+#   threadDrive.cw(motorL, -2400)
 
 gpio.cleanup()
